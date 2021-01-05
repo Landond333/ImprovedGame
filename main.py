150d3227428a3540f950bd964ecb708ce21d89db
Landon D0489 <landond0489@5565.ca>
2021-01-05 21:51:47 +0000

    Added Start, Map, and First Choices.

---
+++ main.py
@@ -0,0 +1,40 @@
+print("Welcome to The Ring.")
+print("Type start to start.")
+start = input()
+if start == "start":
+  print("You, a high ranking member of the royal guard, are tasked with stealing back a priceless ring form a greedy jeweler.")
+  print("With this being a difficult task you've been given many tools to help you along the way.")
+  print("Type Ok to continue.")
+elif start == "Start":
+  print("You, a high ranking member of the royal guard, are tasked with stealing back a priceless ring form a greedy jeweler.")
+  print("With this being a difficult task you've been given many tools to help you along the way.")
+else:
+  print("Failure to choose a correct choice.")
+  exit()
+Ok = input()
+if Ok == "Ok":
+  print("It is known that this jeweler is holding a party for the wealthy tonight, it will be of the best choice for you to be stealthy about your approach.")
+  print("You arrive by the gate and observe the area, pondering how you should begin.")
+else:
+  print("Failure to choose the right choice.")
+  exit()
+print("____________________________________________________________________________________      ________________")
+print("|                                                                                     ^                  |")
+print("|                                                                                     |                  |")
+print("|                                                                              Gate to Garden            |")
+print("|                                                                                                        |")
+print("|                           Yard                                                          Security  ____|")
+print("|                                                                                          Gate     |  ")
+print("|                                                                                          To     ->|")
+print("|                                                                                          House    |")
+print("|_________________________                                                                          |____")
+print("                          |                                              You are Here                   |")
+print("                          |                                                 |                            |")
+print("                          |                                                 V                            |")
+print("                          |_____________________________________________    X    ________________________|") 
+print("Choices:")
+print("1. Go to Garden.\n2. Stay in Yard for a bit.\n3. Go to the Security Gate.")
+
+
+
+
